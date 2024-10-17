db8f067d1481a6df66e8a6089c3b275370929383
lucasa17 <134516774+lucasa17@users.noreply.github.com>
2024-10-17 16:41:04 -0300

    Create ordenacao.java

---
+++ Java/ordenacao.java
@@ -0,0 +1,21 @@
+
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Bem-vindo ao me programa!!!");
+	
+       for(int count=1 ; count <= 10 ; count++){
+        System.out.println(count);
+    }
+       for(int count=10 ; count >=1 ; count--){
+       System.out.println(count);
+       }
+       for(int sobe=1 , desce=10 ; sobe<=10 && desce>=1 ; sobe++ , desce-- ){
+        
+        System.out.printf("%d \t %d \n", sobe , desce);
+    
+    
+    
+}
+	}
+}
